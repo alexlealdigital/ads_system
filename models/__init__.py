1c4aa007dfa1d644c7c60b73514a8641e9fcea01
Alex Leal <51205681+alexlealdigital@users.noreply.github.com>
2025-05-21 17:45:11 -0300

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pacote de modelos para o sistema de anúncios
+Este arquivo __init__.py garante que o diretório models seja reconhecido como um pacote Python
+"""
+
+from models.ads import AdModel
+
+__all__ = ['AdModel']
